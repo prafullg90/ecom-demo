8dc0284fbdc5ea1853efb608c77c555b65ae5ea3
Prafullkumar Gaikwad <prafullkumar.gaikwad@neosoftmail.com>
2023-01-09 00:12:17 +0530

    loader implementation

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,30 @@
 import Head from 'next/head';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import PageLayout from '../components/PageLayout';
+import CircularProgress from '@mui/material/CircularProgress';
+import Grid from '@mui/material/Grid';
+
 import { API } from '../lib/api';
 import { useSelector, useDispatch } from 'react-redux';
 import { productList } from '../redux/Reducers/products';
 import ProductList from '../components/ProductList';
 export default function Home() {
+  const [isLoading, setisLoading] = useState(false);
   const products = useSelector((state) => state.products.products);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    console.log('products', products);
+    // console.log('products', products);
     if (!products.length) {
+      setisLoading(true);
       const cb = {
         success: (res) => {
           dispatch(productList(res));
+          setisLoading(false);
         },
         error: (err) => {
           console.log(err);
+          setisLoading(false);
         },
       };
       const params = {};
@@ -33,7 +40,13 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <PageLayout>
-        <ProductList products={products} />
+        {isLoading ? (
+          <Grid container justifyContent='center'>
+            <CircularProgress />
+          </Grid>
+        ) : (
+          <ProductList products={products} />
+        )}
       </PageLayout>
     </>
   );
